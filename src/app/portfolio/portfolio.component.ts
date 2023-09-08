2707154f8debd51f05f62d87503e851ef0c7ea54
Omar-Zantout <omar82199@gmail.com>
2023-09-08 20:15:40 +0300

    Fix UI add layer

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
 })
 export class PortfolioComponent {
+  imagesUrl = [
+    './../../assets/poert1.png',
+    './../../assets/port2.png',
+    './../../assets/port3.png',
+  ];
+  // imgSelected = new EventEmitter<string>();
+  // logImg(event: Event) {
+  //   console.log(event);
+  // }
+  isLayerVisible: boolean = false;
+  selectedImage: string | null = null;
 
+  toggleLayer(imageUrl: string) {
+    this.selectedImage = imageUrl;
+    this.isLayerVisible = true;
+  }
+
+  hideLayer() {
+    this.isLayerVisible = false;
+    this.selectedImage = null;
+  }
 }
